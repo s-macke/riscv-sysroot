cc6056efb82ef68b88f1d9f28bd5f63184e7e413
Sebastian Macke <sebastian@macke.de>
2017-05-13 21:16:06 +0200

    New toolchain. Fix gcc and binutils

--- fs/usr/lib/gcc/riscv32-unknown-linux-gnu/8.0.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-src/configure --target=riscv32-unknown-linux-gnu --host=riscv32-unknown-linux-gnu --prefix=/usr --program-prefix= --disable-debug --disable-libssp --enable-shared=libstdc++,libgomp,libatomic,libsupc++ --srcdir=../gcc-src --enable-languages=c --enable-threads=posix --disable-libquadmath --disable-libmudflap --disable-multilib --disable-bootstrap --disable-libsanitizer --disable-nls --disable-lto --with-system-zlib --disable-docs --with-target-libiberty=no";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "ilp32d" }, { "arch", "rv32gc" } };
